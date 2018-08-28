5620da2b2263cbcabe29567e016371803e88eb9a
newmane <emily.newman@levvel.io>
2018-08-27 21:14:26 -0400

    Initial Commit

---
+++ FitnessTracker/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  FitnessTracker
+//
+//  Created by Emily Newman on 8/27/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
